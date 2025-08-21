c88b271733f102d9bc4dc0de3647e02f4d08f89f
Tony <tony.wu@myzeller.com>
2025-08-21 15:03:26 +1000

    feat: new version

--- apps/index.js
+++ apps/index.js
@@ -1,3 +1,3 @@
 console.log('This is the index file for the apps directory.')
-console.log('You can add your application logic here.')
+console.log('You can add your application logic here. 123123')
 console.log('You can add your application logic here. 123444')
