b64c88e72e04438a11b466fe3dee8f8e420d7220
Tony <tony.wu@myzeller.com>
2025-08-28 14:25:39 +1000

    fix: chagne 2 after commit-and-tag-version

--- apps/index.js
+++ apps/index.js
@@ -1,4 +1,4 @@
 console.log('This is the index file for the apps directory. 113123')
 console.log('You can add your application logic here. 123123')
 console.log('You can add your application logic here. 1sdsf')
-console.log('some changes')
+console.log('some changes 2')
